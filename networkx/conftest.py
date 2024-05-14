b3758a8f0e15c39595570379d50ea7f59b520366
Matt Schwennesen <mjschwenne@gmail.com>
2024-05-14 11:45:48 -0500

    remove edmonds class

--- networkx/conftest.py
+++ networkx/conftest.py
@@ -11,6 +11,7 @@ General guidelines for writing good tests:
   and add the module to the relevant entries below.
 
 """
+
 import os
 import sys
 import warnings
@@ -101,9 +102,6 @@ def set_warnings():
     )
     warnings.filterwarnings(
         "ignore", category=DeprecationWarning, message="\n\nrandom_tree"
-    )
-    warnings.filterwarnings(
-        "ignore", category=DeprecationWarning, message="Edmonds has been deprecated"
     )
     warnings.filterwarnings(
         "ignore",
